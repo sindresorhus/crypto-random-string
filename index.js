1b713751d875ce1aa0f6e54bb313faf3b70d7445
keenondrums <frantic1993@gmail.com>
2017-10-09 10:35:01 -0600

    Fixed typos

--- index.js
+++ index.js
@@ -2,19 +2,17 @@
 const crypto = require('crypto');
 const util = require('util');
 
-
-const randomBytesAsync = util.promisify(crypto.randomBytes)
+const randomBytesAsync = util.promisify(crypto.randomBytes);
 
 const getBytes = randomBytesFn => len => {
 	if (!Number.isFinite(len)) {
 		throw new TypeError('Expected a finite number');
 	}
-	
-	return randomBytesFn(Math.ceil(len / 2))
-}
 
-const normalize = bytes => bytes.toString('hex').slice(0, len))
+	return randomBytesFn(Math.ceil(len / 2));
+};
 
-module.exports.sync = len => normalize(getBytes(crypto.randomBytes));
+const normalize = (len, bytes) => bytes.toString('hex').slice(0, len);
 
-module.exports.async = len => getBytes(randomBytesAsync).then(normalize);
+module.exports.sync = len => normalize(len, getBytes(crypto.randomBytes)(len));
+module.exports.async = len => getBytes(randomBytesAsync)(len).then(bytes => normalize(len, bytes));
