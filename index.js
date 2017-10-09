3a9c6f9e41b7b41230e3ae101f09f476aa9ef6d3
Andrei Goncharov <frantic1993@gmail.com>
2017-10-09 10:15:54 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 'use strict';
 const crypto = require('crypto');
+const util = require('util');
 
-module.exports = len => {
+
+const randomBytesAsync = util.promisify(crypto.randomBytes)
+
+const getBytes = randomBytesFn => len => {
 	if (!Number.isFinite(len)) {
 		throw new TypeError('Expected a finite number');
 	}
+	
+	return randomBytesFn(Math.ceil(len / 2))
+}
 
-	return crypto.randomBytes(Math.ceil(len / 2)).toString('hex').slice(0, len);
-};
+const normalize = bytes => bytes.toString('hex').slice(0, len))
+
+module.exports.sync = len => normalize(getBytes(crypto.randomBytes));
+
+module.exports.async = len => getBytes(randomBytesAsync).then(normalize);
