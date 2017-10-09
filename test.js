d2b6546178d7478b81742c91e62453f6007c0419
Andrei Goncharov <frantic1993@gmail.com>
2017-10-09 10:24:38 -0600

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,17 @@
 import test from 'ava';
 import m from './';
 
-test(t => {
-	t.is(m(0).length, 0);
-	t.is(m(10).length, 10);
-	t.is(m(100).length, 100);
+test('sync', t => {
+	t.is(m.sync(0).length, 0);
+	t.is(m.sync(10).length, 10);
+	t.is(m.sync(100).length, 100);
+});
+
+test('async', async t => {
+	const m0 = await m.async(0);
+	t.is(m0.length, 0);
+	const m10 = await m.async(10);
+	t.is(m10.length, 10);
+	const m100 = await m.async(100);
+	t.is(m100.length, 100);
 });
