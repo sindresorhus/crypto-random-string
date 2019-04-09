24c52c413f0ee5dcbabe400c349c8d47a7157302
Dimitri Benin <dimitri.benin@gmail.com>
2019-04-09 20:51:18 +0200

    Require Node.js 8, add TypeScript definition

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 import test from 'ava';
-import m from './';
+import cryptoRandomString from '.';
 
-test(t => {
-	t.is(m(0).length, 0);
-	t.is(m(10).length, 10);
-	t.is(m(100).length, 100);
+test('main', t => {
+	t.is(cryptoRandomString(0).length, 0);
+	t.is(cryptoRandomString(10).length, 10);
+	t.is(cryptoRandomString(100).length, 100);
 });
