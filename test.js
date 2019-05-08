71be0c097f21ad8f055f2ba559147fb3ce8f6647
Sindre Sorhus <sindresorhus@gmail.com>
2019-05-08 16:21:43 +0700

    Update test.js

--- test.js
+++ test.js
@@ -5,28 +5,28 @@ test('main', t => {
 	t.is(cryptoRandomString(0).length, 0);
 	t.is(cryptoRandomString(10).length, 10);
 	t.is(cryptoRandomString(100).length, 100);
-	t.regex(cryptoRandomString(100), /^[0-9a-f]*$/); // Sanity check, probabilistic
+	t.regex(cryptoRandomString(100), /^[a-f\d]*$/); // Sanity check, probabilistic
 });
 
 test('hex', t => {
 	t.is(cryptoRandomString(0, {type: 'hex'}).length, 0);
 	t.is(cryptoRandomString(10, {type: 'hex'}).length, 10);
 	t.is(cryptoRandomString(100, {type: 'hex'}).length, 100);
-	t.regex(cryptoRandomString(100, {type: 'hex'}), /^[0-9a-f]*$/); // Sanity check, probabilistic
+	t.regex(cryptoRandomString(100, {type: 'hex'}), /^[a-f\d]*$/); // Sanity check, probabilistic
 });
 
 test('base64', t => {
 	t.is(cryptoRandomString(0, {type: 'base64'}).length, 0);
 	t.is(cryptoRandomString(10, {type: 'base64'}).length, 10);
 	t.is(cryptoRandomString(100, {type: 'base64'}).length, 100);
-	t.regex(cryptoRandomString(100, {type: 'base64'}), /^[a-zA-Z0-9/+]*$/); // Sanity check, probabilistic
+	t.regex(cryptoRandomString(100, {type: 'base64'}), /^[a-zA-Z\d/+]*$/); // Sanity check, probabilistic
 });
 
 test('url-safe', t => {
 	t.is(cryptoRandomString(0, {type: 'url-safe'}).length, 0);
 	t.is(cryptoRandomString(10, {type: 'url-safe'}).length, 10);
 	t.is(cryptoRandomString(100, {type: 'url-safe'}).length, 100);
-	t.regex(cryptoRandomString(100, {type: 'url-safe'}), /^[a-zA-Z0-9._~-]*$/); // Sanity check, probabilistic
+	t.regex(cryptoRandomString(100, {type: 'url-safe'}), /^[a-zA-Z\d._~-]*$/); // Sanity check, probabilistic
 });
 
 test('characters', t => {
@@ -37,8 +37,19 @@ test('characters', t => {
 });
 
 test('argument errors', t => {
-	t.throws(() => cryptoRandomString(Infinity));
-	t.throws(() => cryptoRandomString(0, {type: 'hex', characters: '1234'}));
-	t.throws(() => cryptoRandomString(0, {characters: 42}));
-	t.throws(() => cryptoRandomString(0, {type: 'unknown'}));
+	t.throws(() => {
+		cryptoRandomString(Infinity);
+	});
+
+	t.throws(() => {
+		cryptoRandomString(0, {type: 'hex', characters: '1234'});
+	});
+
+	t.throws(() => {
+		cryptoRandomString(0, {characters: 42});
+	});
+
+	t.throws(() => {
+		cryptoRandomString(0, {type: 'unknown'});
+	});
 });
