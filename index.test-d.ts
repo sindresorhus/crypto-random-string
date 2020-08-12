0584ce78bc54edc231718f68d9d030cc64405d97
Richie Bendall <richiebendall@gmail.com>
2020-08-13 02:50:43 +1200

    Update tests
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

--- index.test-d.ts
+++ index.test-d.ts
@@ -5,6 +5,7 @@ expectType<string>(cryptoRandomString({length: 10}));
 expectType<string>(cryptoRandomString({length: 10, type: 'url-safe'}));
 expectType<string>(cryptoRandomString({length: 10, type: 'numeric'}));
 expectType<string>(cryptoRandomString({length: 10, characters: '1234'}));
+expectType<Promise<string>>(cryptoRandomString.async({ length: 10 }));
 
 expectError(cryptoRandomString({type: 'url-safe'}));
 expectError(cryptoRandomString({length: 10, type: 'url-safe', characters: '1234'}));
