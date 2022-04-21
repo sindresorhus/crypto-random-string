4f69e0bc9352d355e5979aca98d647fd17d9a5ff
Richie Bendall <richiebendall@gmail.com>
2022-04-21 16:21:35 +1200

    Add todo
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// TODO: When targeting Node.js 16, use `crypto.webcrypto.getRandomValues` to interop with the browser code and when targeting Node.js 18, only use the browser code
 import {promisify} from 'node:util';
 import crypto from 'node:crypto';
 import {createStringGenerator, createAsyncStringGenerator} from './core.js';
